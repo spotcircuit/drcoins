1013867b2e0de0a7c8a39b7799a86269ab59dea2
OhDeerCoins <user@ohdeercoins.com>
2025-09-02 16:03:54 -0400

    new version

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
   reactStrictMode: true,
   images: {
     domains: ['your-cdn.com', 'images.unsplash.com'], // Add your image domains here
